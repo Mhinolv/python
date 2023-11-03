027ebe064db4bf6062a8618c1390e9debd2315b1
Mhinoverse <Mhinolv@gmail.com>
2023-11-02 21:50:27 -0700

    adding Variables notebook and .Py to repo

---
+++ Python/Variables/Variables.py
@@ -0,0 +1,20 @@
+## Integer Variables
+
+a = 2
+b = 4
+
+## Operations on Integer Variables
+a + b
+a - b
+b % a
+
+##String Variables
+
+c = 'Cat'
+d = 'Dog'
+c + d
+
+## Lists
+e = [1,2,3]
+d = [5,6,7]
+e + d
